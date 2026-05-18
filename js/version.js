00c3c3b59281f150c23b3209e3f29e567d10d3b2
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-05-18 16:45:49 +0900

    add admin book id help

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.58";
-export const BUILD_TIME = "2026-05-18 16:26";
-export const COMMIT = "admin-storage-lite-analytics";
+export const APP_VERSION = "0.1.59";
+export const BUILD_TIME = "2026-05-18 16:44";
+export const COMMIT = "admin-id-help";
