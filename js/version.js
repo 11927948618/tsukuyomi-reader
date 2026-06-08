56a3f7cd7f79df09ed71d2d77b2c54cc3eee6595
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-08 16:42:17 +0900

    pin mobile pager text to frame

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.100";
-export const BUILD_TIME = "2026-06-08 16:42";
+export const APP_VERSION = "0.1.101";
+export const BUILD_TIME = "2026-06-08 16:55";
 export const COMMIT = "review-auth-retrospective-log";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.100";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.101";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
