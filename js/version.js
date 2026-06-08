69a125a1a3f106bc29fc888768d601683e72d06b
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-08 16:24:44 +0900

    fill mobile pager width

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.98";
-export const BUILD_TIME = "2026-06-08 16:18";
+export const APP_VERSION = "0.1.99";
+export const BUILD_TIME = "2026-06-08 16:28";
 export const COMMIT = "review-auth-retrospective-log";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.98";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.99";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
