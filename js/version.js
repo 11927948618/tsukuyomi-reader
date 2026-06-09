522a6af168c8eadeced17c64d9e51fe348e8b88a
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-09 11:03:04 +0900

    Keep mobile reader frame full height

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.109";
-export const BUILD_TIME = "2026-06-09 11:05";
+export const APP_VERSION = "0.1.110";
+export const BUILD_TIME = "2026-06-09 11:20";
 export const COMMIT = "review-auth-retrospective-log";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.109";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.110";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
