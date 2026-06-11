269612b8e1f6c9ccd72cfde1d6cf251d81f2e286
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-11 16:04:17 +0900

    Stabilize mobile paged frame offset

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.115";
-export const BUILD_TIME = "2026-06-11 15:35";
+export const APP_VERSION = "0.1.116";
+export const BUILD_TIME = "2026-06-11 15:55";
 export const COMMIT = "review-auth-retrospective-log";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.115";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.116";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
