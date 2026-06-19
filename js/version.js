ac588161161b14f73592676aba8338e1e54cb631
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-19 15:46:19 +0900

    Increase reader topbar button contrast

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.145";
+export const APP_VERSION = "0.1.146";
 export const BUILD_TIME = "2026-06-19 00:00";
-export const COMMIT = "settings-button-borders";
+export const COMMIT = "reader-topbar-borders";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.145";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.146";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
