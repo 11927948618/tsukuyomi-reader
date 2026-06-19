459db0b3bd4bbaeca308321278975b3f6bb2ac8b
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-19 15:53:32 +0900

    Unify Library and settings button borders

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.146";
+export const APP_VERSION = "0.1.147";
 export const BUILD_TIME = "2026-06-19 00:00";
-export const COMMIT = "reader-topbar-borders";
+export const COMMIT = "library-settings-borders";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.146";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.147";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
