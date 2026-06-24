71464d55353aa55bcdba8afff47a836602d9afe4
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-24 16:35:48 +0900

    Fix spread grid writing mode

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.157";
+export const APP_VERSION = "0.1.158";
 export const BUILD_TIME = "2026-06-24 00:00 JST";
-export const COMMIT = "fix-vertical-column-spread";
+export const COMMIT = "fix-spread-grid-writing-mode";
