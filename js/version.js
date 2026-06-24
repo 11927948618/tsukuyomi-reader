6b0a765e1de792cbc8b20736b86cd66a163e3fc9
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-06-24 11:18:56 +0900

    Compact library book cards

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.153";
-export const BUILD_TIME = "2026-06-22 17:01 JST";
-export const COMMIT = "public-smoke-tests";
+export const APP_VERSION = "0.1.154";
+export const BUILD_TIME = "2026-06-24 00:00 JST";
+export const COMMIT = "compact-library-cards";
