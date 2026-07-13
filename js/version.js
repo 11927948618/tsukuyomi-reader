2d6e85c62e77e749e23e7dc19c363beb2651d2eb
halthejuggernaut-cyber <halthejuggernaut@gmail.com>
2026-07-13 17:33:46 +0900

    backup 2026/07/13 17:33:46

--- js/version.js
+++ js/version.js
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.1.202";
-export const BUILD_TIME = "2026-07-13 17:09 JST";
-export const COMMIT = "backup-202607131709";
+export const APP_VERSION = "0.1.205";
+export const BUILD_TIME = "2026-07-13 17:33 JST";
+export const COMMIT = "backup-202607131733";
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tsukuyomi-reader-v0.1.202";
+const CACHE_NAME = "tsukuyomi-reader-v0.1.205";
 const STATIC_ASSETS = [
   "./",
   "./index.html",
